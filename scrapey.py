f76963894cd78deeb12a9f549bd27b59688dbb73
Janet <perezjanet103@gmail.com>
2021-09-22 23:59:30 -0400

    making fixing flake8 errors

--- scrapey.py
+++ scrapey.py
@@ -3,40 +3,42 @@ from bs4 import BeautifulSoup
 from app import db, Covid19Table
 
 def scrape():
-    url ="https://en.wikipedia.org/wiki/COVID-19_pandemic_cases"
+    url="https://en.wikipedia.org/wiki/COVID-19_pandemic_cases"
 
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "html.parser")
     table = soup.findAll('tr')
-    
     data = [list(item.stripped_strings) for item in table[31:-46]]
     world = [list(item.stripped_strings) for item in table[28:29]]
-    
     for lst in data:
-        for i,item in enumerate(lst[2:]):
+        for i, item in enumerate(lst[2:]):
             if type(item) == str:
-                lst[i+2] = int(item.replace(",", ''))
-                
+                lst[i+2] = int(item.replace(",", ''))         
     for lst in world:
-        for i,item in enumerate(lst[1:]):
+        for i, item in enumerate(lst[1:]):
             if type(item) == str:
-                lst[i+1] = int(item.replace(",", ''))
-                
+                lst[i+1] = int(item.replace(",", ''))   
     lst_world = [num for item in world for num in item]
                 
-    latin_america = ["USA","Belize", "Costa Rica", "El Salvador", "Guatemala", "Honduras", "Mexico", "Nicaragua", "Panama",
-                    "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "French Guiana", "Guyana", 
-                     "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"]
+    latin_america = ["USA", "Belize", "Costa Rica", "El Salvador",
+                     "Guatemala", "Honduras", "Mexico", "Nicaragua", "Panama",
+                     "Argentina", "Bolivia", "Brazil", "Chile", "Colombia",
+                     "Ecuador", "French Guiana", "Guyana", 
+                     "Paraguay", "Peru", "Suriname", "Uruguay", 
+                     "Venezuela"]
     
     americas_data = [lst for lst in data if lst[0] in latin_america]
 
-    #PERCENTAGE OF COVID-19 CASES IN LATIN AMERICA INCLUDING USA COMPARED TO THE REST OF THE WORLD
+    '''PERCENTAGE OF COVID-19 CASES IN LATIN AMERICA INCLUDING 
+    USA COMPARED TO THE REST OF THE WORLD'''
     for lst in americas_data:
         for i,num in enumerate(lst[2:]):
             lst[i+2]= round((num / lst_world[i+1]) * 100, 4)     
     return americas_data
     
 def setup_db():
+    """[summary]
+    """    
     data = scrape()
     db.drop_all()
     db.create_all()
